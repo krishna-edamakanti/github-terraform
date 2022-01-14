2310eb9b048015056cefc2ab7259d6669e28c8b1
krishna-edamakanti <95684277+krishna-edamakanti@users.noreply.github.com>
2022-01-14 11:04:09 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -12,3 +12,19 @@ variable "sname" {
   description = "Azure Storage Account"
   type        = string
 }
+variable "cluster_name" {
+  type        = string
+  description = "AKS name in Azure"
+}
+variable "kubernetes_version" {
+  type        = string
+  description = "Kubernetes version"
+}
+variable "system_node_count" {
+  type        = number
+  description = "Number of AKS worker nodes"
+}
+variable "node_resource_group" {
+  type        = string
+  description = "RG name for cluster resources in Azure"
+}
