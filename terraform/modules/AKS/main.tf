3901220f1953399c6f2e96ae185aa37d5328becb
krishna-edamakanti <95684277+krishna-edamakanti@users.noreply.github.com>
2022-01-14 12:33:45 +0530

    Update main.tf

--- terraform/modules/AKS/main.tf
+++ terraform/modules/AKS/main.tf
@@ -2,7 +2,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   name                = var.cluster_name
   kubernetes_version  = var.kubernetes_version
   location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name = var.rgname
   dns_prefix          = var.cluster_name
   node_resource_group = var.node_resource_group
 
